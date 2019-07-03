bbd6091aa96bd2567f0514121e9520233e69c336
teosposib <teodor.sposib@gmail.com>
2019-07-03 17:32:36 +0300

    Use expression to change the content of component

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,20 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      {{ title }} {{number1 + number2 }}
+    </div>
+    <div>
+      {{ isHappy ? ':)' : ':(' }}
     </div>
   `
 })
 export class AppComponent {
+  title: string;
+  number1: number = 1;
+  number2: number = 2;
+  isHappy: boolean = false;
 
+  constructor() {
+    this.title = "Title"
+  }
 }
