e58e774897711119d5553b36fb44c415c0ab56c4
teosposib <teodor.sposib@gmail.com>
2019-07-04 10:01:25 +0300

    Use events binding in app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,13 +6,29 @@ import { Component } from '@angular/core';
   template: `
     <div class="app">
       <h1 [innerHTML]="title"></h1>
-      <input type="text" [value]="name">
+      <input 
+        type="text" 
+        [value]="name"
+        (input)=eventInput($event)>
+      <div> {{ name }} </div>
+      <button 
+        (click)=eventClick()>
+        Change name
+      </button>
     </div>
   `
 })
 export class AppComponent {
   name: string = 'Name';
   title: string;
+
+  eventClick(){
+    this.name = "Name";
+  }
+
+  eventInput(event: any) {
+    this.name = event.target.value;
+  }
 
   constructor() {
     this.title = "Title"
