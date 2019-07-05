f6f035aa8517ded813c46595a87b9118ef3d5578
teosposib <teodor.sposib@gmail.com>
2019-07-05 12:38:46 +0300

    Create home component for the main page

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <passanger-viewer></passanger-viewer>
+      <router-outlet></router-outlet>
     </div>
   `
 })
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common'
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 import { PassangerDashboardModule } from './passanger-dashboard/passanger-dashboard.module';
 
+import { HomeComponent } from './home.component';
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [
     BrowserModule,
     CommonModule,
-    PassangerDashboardModule
+    PassangerDashboardModule,
+    RouterModule.forRoot(routes)
   ],
   bootstrap: [
     AppComponent
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ]
 })
 export class AppModule {}
